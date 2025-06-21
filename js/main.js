ea3d50c1468e312dbf6b9d88983b2fcbd3848198
GeovannaLSouza <geovannamorila@gmail.com>
2025-06-21 18:15:16 -0300

    first lesson js

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Strings
+const myVariable = "Mathematics";
+
+
+// the length property
+//console.log(myVariable.length);
+
+// the length property
+//console.log("Every good boy does fine".length);
+
+//String Methods
+//console.log(myVariable.charAt(6));
+
+//String Medhods
+//console.log(myVariable.indexOf("Mat"));
+
+//String Medhods
+//console.log(myVariable.lastIndexOf("at"));
+
+//String Medhods
+//console.log(myVariable.slice(5, 8));
+
+//String Medhods
+//console.log(myVariable.toUpperCase());
+
+//String Medhods
+//console.log(myVariable.toLowerCase());
+
+//String Medhods
+//console.log(myVariable.includes("div"));
+
+//String Medhods
+//console.log(myVariable.includes("mat"));
+
+//String Medhods
+//console.log(myVariable.split("e"));
+
+//String Medhods
+//console.log(myVariable.split(""));
+
+//String Medhods
+//console.log("John,Joe,Dave".split(","));
+
+//String Medhods
+//console.log("Every godd boy does fine".split(" "));
