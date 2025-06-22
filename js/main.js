9b729438215229d0449b5c528b2be738025441d6
GeovannaLSouza <geovannamorila@gmail.com>
2025-06-22 16:38:43 -0300

    Detailed explanatory description of how strings work

--- js/main.js
+++ js/main.js
@@ -1,47 +1,70 @@
 // Strings
-const myVariable = "Mathematics";
+//const myVariable = "Mathematics"; (Vai imprimir: Mathematics)
 
 
 // the length property
-//console.log(myVariable.length);
+//console.log(myVariable.length); (propriedade da string. Ela retorna o número de caracteres
+//(letras, números, espaços, tudo oque a string tem)
+
 
 // the length property
-//console.log("Every good boy does fine".length);
+//console.log("Every good boy does fine".length); (Mesma situação de cima)
+
 
 //String Methods
-//console.log(myVariable.charAt(6));
+//console.log(myVariable.charAt(6)); (comando que mostra qual caractere está na posição 6 da string myVariable,
+//porém se alterar o número dentro do parenteses, irá alterar também a posição que o programa irá analisar a posição
+//que estará o caractere)
+
 
 //String Medhods
-//console.log(myVariable.indexOf("Mat"));
+//console.log(myVariable.indexOf("Mat")); (comando que procura a posição 
+//onde o texto "Mat" começa dentro da string armazenada em myVariable, que é "Mathematics".)
+
 
 //String Medhods
-//console.log(myVariable.lastIndexOf("at"));
+//console.log(myVariable.lastIndexOf("at")); (Mesma situação de cima)
+
 
 //String Medhods
-//console.log(myVariable.slice(5, 8));
+//console.log(myVariable.slice(5, 8)); (comando recorta um trecho da string myVariable, pegando os caracteres do índice 5 até o 7
+//(o índice final não é incluído))
+
 
 //String Medhods
-//console.log(myVariable.toUpperCase());
+//console.log(myVariable.toUpperCase()); (Não altera a string original, apenas retorna uma nova versão dela em maiúsculas)
+
 
 //String Medhods
-//console.log(myVariable.toLowerCase());
+//console.log(myVariable.toLowerCase()); (Também não altera a string original; ele retorna uma nova versão com tudo em minúsculo)
+
 
 //String Medhods
-//console.log(myVariable.includes("div"));
+//console.log(myVariable.includes("div")); (comando verifica se a string myVariable (que é "Mathematics") contém o texto "div". Ele retorna:
+//true se encontrar o texto dentro da string; false se não encontrar.)
+
 
 //String Medhods
-//console.log(myVariable.includes("mat"));
+//console.log(myVariable.includes("mat")); (Mesma situação de cima)
+
 
 //String Medhods
-//console.log(myVariable.split("e"));
+//console.log(myVariable.split("e")); (Esse comando divide a string myVariable em um array de pedaços, usando a letra "e"
+//como ponto de corte. Ou seja: Cada vez que encontrar a letra "e", ele corta a string e cria uma parte nova.
+//Essas partes ficam dentro de um array (lista de elementos)).
+
 
 //String Medhods
-//console.log(myVariable.split(""));
+//console.log(myVariable.split("")); (Esse comando divide a string myVariable em cada letra separada, porque o separador que você passou é uma string vazia "".)
+
 
 //String Medhods
-//console.log("John,Joe,Dave".split(","));
+//console.log("John,Joe,Dave".split(",")); (string "John,Joe,Dave" toda vez que encontra uma vírgula ",", criando um array com as partes separadas.)
+
 
 //String Medhods
-//console.log("Every godd boy does fine".split(" "));
+//console.log("Every good boy does fine".split(" ")); (um array onde cada palavra vira um elemento separado.)
+
 
 //number
+
